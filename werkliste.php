ba10af5109fd910266cc03f4eb888c6b04079724
Ivo Schwizer <ivoschwizer@gmail.com>
2023-07-26 08:31:48 +0200

    u

---
+++ werkliste.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="de">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# place: http://ogp.me/ns/place#">
+    <meta charset="utf-8">
+    <title>Swiss Baumanagement | Projekte</title>
+    <meta name="description" content="Swiss Baumanagement AG Projektübersicht">
+    <?php include 'head.php'; ?>
+</head>
+  
+<body>
+    <?php include 'header.php'; ?>
+    <section class="werkcontainer">
+        <div class="acccontainer">
+            <div class="accordion">
+                <h1 class="accTitle">Mehrfamilienhäuser</h1>
+                <div class="accList">
+                    <ul>
+                        <li class="wtitle">2014</li>
+                        <li><span>Neubau Mehrfamilienhaus Buchs</span><span>Werkplanung</span></li>
+                        <li class="wtitle">2015</li>
+                        <li><span>Neubau 2 Mehrfamilienhäuser (16 Wohnungen) Balterswil</span><span>Werkplanung</span></li>
+                        <li><span>Neubau 6-Familienhaus Uzwil</span><span>Projektplanung</span></li>
+                        <li><span>Überbauung 7 MFH (57 Wohnungen) Eggli Abtwil</span><span>Baumanagement</span></li>
+                        <li class="wtitle">2016</li>
+                        <li><span>Überbauung (MFH + 4 EFH) Römerstrasse Arbon</span><span>Projektstudie</span></li>
+                        <li class="wtitle">2017</li>
+                        <li><span>Neubau 6-Familienhaus Amriswil</span><span>Projektstudie</span></li>
+                        <li class="wtitle">2018</li>
+                        <li><span>Neubau 5-Familienhaus Säntisstrasse Amriswil</span><span>Projekt- und Werkplanung</span></li>
+                        <li class="wtitle">2019</li>
+                        <li><span>Neubau 12-Familienhaus Staad</span><span>Projekt- und Werkplanung</span></li>
+                        <li><span>Neubau 14-Familienhaus Goldach</span><span>Projekt- und Werkplanung, Ausschreibung</span></li>
+                        <li><span>Neubau Mehrfamilienhaus Gommiswald</span><span>Projektstudie</span></li>
+                        <li class="wtitle">2020</li>
+                        <li><span>Neubau 11-Familienhaus Rappensteinstrasse St.Gallen</span><span>Projektplanung</span></li>
+                        <li><span>Neubau Mehrfamilienhaus Terminusstrasse Goldach</span><span>Projektplanung</span></li>
+                        <li><span>Neubau MFH Romanshornerstrasse Amriswil</span><span>Projektplanung</span></li>
+                    </ul>
+                </div>
+
+                
+
+
+
+                <h1 class="accTitle">Gewerbehäuser</h1>
+                <div class="accList">
+                    <p>Die Swiss Baumanagement AG erbringt sämtliche Teilleistungen von der ersten Entwurfsidee über alle Planungsphasen in gestalterischer, konstruktiver und ökonomischer Höchstqualität. Möglich machen dies unsere langjährige Erfahrung und die fundierte Fachkompetenz unserer Mitarbeitenden. Die Projektierung von Um-und Neubauten ist unsere Leidenschaft. Da jede Aufgabe einzigartig ist, fühlen wir uns keiner speziellen Architektursprache verpflichtet, sondern suchen mit jeder Bauherrschaft und jedem neuen Ort eine persönliche und individuelle Lösung! Wir suchen innerhalb der gegebenen Möglichkeiten nach innovativen Ideen welche wir mit Ihnen zusammen weiter entwickeln möchten.</p>
+                </div>
+                <h1 class="accTitle">Einfamilienhäuser / Villa</h1>
+                <div class="accList">
+                    <p>In der Bauleitung legen wir nebst der technisch einwandfreien Umsetzung unserer Bauprojekte, grossen Wert auf ein klares und transparentes Zeit- und Kostenmanagement. So stehen wir von der Projektierung bis zur Umsetzung in einem engen partnerschaftlichen Verhältnis mit unseren Bauherrschaften. So versuchen wir bis zur Schlüsselübergabe wo immer möglich je nach Baufortschritt flexibel auf Wünsche und Bedürfnisse unserer Kunden einzugehen und in der Umsetzung berücksichtigen zu können.</p>
+                </div>
+                <h1 class="accTitle">Um- und Anbauten</h1>
+                <div class="accList">
+                    <p>Die Ausführungsplanung bildet für uns die selbstverständliche und konsequente Fortsetzung der vorangegangenen Projektierungsarbeit. Die technische Ausarbeitung und einwandfreie Planung bildet die Grundlage für die reibungslose Umsetzung in der folgenden Bauphase.</p>
+                </div>
+                <h1 class="accTitle">Wettbewerbe</h1>
+                <div class="accList">
+                    <p>Die Swiss Baumanagement für Sie die Leitung der Garantiearbeiten und sorgt für die detailgenaue Erfassung und fristgerechte Beseitigung der vorhandenen Mängel. Die fachkundige Leitung der Garantiearbeiten schützt Sie vor kostenintensiven Nachbesserungen und setzt Ihre Rechte für die Mängelbeseitigung innerhalb der gesetzlichen Garantiefristen durch.</p>
+                </div>
+            </div>
+        </div>
+        
+    </section>
+    <?php include 'script.php'; ?>
+</body>
+</html>
